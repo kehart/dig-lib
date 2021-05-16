ca1ab155dc553d8ad15d97b7eb28a26a710dc66d
Katherine Hart <kasiahart@yahoo.ca>
2021-05-16 17:54:52 -0400

    Added more endpointsT

--- server/db/index.js
+++ server/db/index.js
@@ -13,9 +13,12 @@ const pool = mysql.createPool({
 
 let libDb = {};
 
+/**
+ * User transactions
+ */
+
 libDb.insertUser = (user_data) => {
     user_id = uuid.v4();
-    console.log(user_id.length)
     hashed_pword = sha256(user_data.password.toString());
     vals = [user_id, user_data.first_name, user_data.last_name, user_data.email, user_data.display_name, user_data.user_name, hashed_pword]
     return new Promise((resolve, reject) => { 
@@ -50,6 +53,10 @@ libDb.getUserById = (user_id) => {
     });
 }
 
+/**
+ * Library transactions
+ */
+
 libDb.getLibWithName = (owner_id, name) => {
     return new Promise((resolve, reject) => {
         pool.query(`SELECT * FROM libraries WHERE owner_id = ? AND lib_name = ?`, [owner_id, name], (err, results) => {
@@ -74,6 +81,38 @@ libDb.createLib = (owner_id, name) => {
     })
 }
 
+/**
+ * Author transactions
+ */
+
+libDb.getAuthor = (given_name, last_name) => {
+    return new Promise((resolve, reject) => { 
+        pool.query(`SELECT * FROM authors WHERE given_name = ? and last_name = ?;`, [given_name, last_name], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        })
+    })
+}
+
+libDb.insertAuthor = (given_name, last_name) => {
+    author_id = uuid.v4();
+    vals = [author_id, given_name, last_name]
+    return new Promise((resolve, reject) => { 
+        pool.query(`INSERT INTO authors VALUES (?, ?, ?)`, vals, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(author_id);
+        })
+    })
+}
+
+/**
+ * Book transactions
+ */
+
 libDb.all = () => {
 
     return new Promise((resolve, reject) => {
@@ -86,6 +125,28 @@ libDb.all = () => {
     })
 };
 
+libDb.booksByTitle = (title, subtitle) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM books WHERE title = ? AND subtitle= ?;`, [title, subtitle], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        })
+    })
+}
+
+libDb.getBookAuthors = (bookId) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM book_author WHERE book_id = ?;`, [bookId], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        })
+    })
+}
+
 libDb.one = (id) => {
     return new Promise((resolve, reject) => {
         pool.query(`SELECT * FROM books WHERE book_id = ?;`, [id], (err, results) => {
@@ -96,6 +157,65 @@ libDb.one = (id) => {
         });
     })
 };
+
+libDb.insertBook = (title, subtitle, pub_date, publisher, edition, cover_url, genre) => {
+    bookId = uuid.v4();
+    vals = [bookId, title, subtitle, pub_date, publisher, edition, cover_url, genre]
+    return new Promise((resolve, reject) => { 
+        pool.query(`INSERT INTO books VALUES (?, ?, ?, ?, ?, ?, ?, ?)`, vals, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(bookId);
+        })
+    })
+}
+
+libDb.insertBookAuthor = (bookId, authorId) => {
+    return new Promise((resolve, reject) => { 
+        pool.query(`INSERT INTO book_author VALUES (?, ?)`, [bookId, authorId], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        })
+    })
+}
+
+libDb.insertBookIntoLibrary = (bookId, libraryId) => {
+    values = [libraryId, bookId, 'UNREAD']
+    return new Promise((resolve, reject) => { 
+        pool.query(`INSERT INTO library_books VALUES (?, ?, ?)`, values, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        })
+    })
+}
+
+libDb.insertGenre = (genre) => {
+    genre_id = uuid.v4();
+    return new Promise((resolve, reject) => { 
+        pool.query(`INSERT INTO genre VALUES (?, ?)`, [genre_id, genre], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(genre_id);
+        })
+    });
+}
+
+libDb.getUserLibraries = (userId) => {
+    return new Promise((resolve, reject) => { 
+        pool.query(`SELECT * FROM libraries WHERE owner_id = ?`, userId, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        })
+    })
+}
 
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const db = require('../db')
 const validator = require('../utils')
 const sha256 = require('js-sha256');
+const { json } = require('express');
 
 /**
  * Register a new user
@@ -59,14 +60,14 @@ router.post('/login', async (req, res, next) => {
             res.statusCode = 400
             res.json({'error': `no user with username ${body.user_name} exists`})
         }
-        console.log(users[0])
-    
+
+        // Compare passwords to see if valid login
         stored_password_hash = users[0].pword
         given_password_hash = sha256(body.password.toString())
     
         if (stored_password_hash === given_password_hash) {
             res.statusCode = 200
-            res.json({'status': 'success'}) // later pass a token
+            res.json({'status': 'success', 'user_id': users[0].user_id}) // later pass a token
         } else {
             res.statusCode = 400
             res.json({'error': 'invalid password'})
@@ -98,11 +99,15 @@ router.post('/libraries', async (req, res, next) => {
         let users = await db.getUserById(body.owner_id)
         if (users.length === 0) {
             // error no such user exists
+            res.statusCode = 403
+            res.json({'error': `action forbidden for user`});
         }
         // check that this record does not yet exist
         let existingLibs = await db.getLibWithName(body.owner_id, body.lib_name);
         if (existingLibs.length > 0) {
             // error, this library already exists for user
+            res.statusCode = 400
+            res.json({'error': `library with name ${body.lib_name} already exists for user`})
         }
         // insert record
         await db.createLib(body.owner_id, body.lib_name)
@@ -114,17 +119,115 @@ router.post('/libraries', async (req, res, next) => {
     }
 })
 
+/** 
+ * Get all libraries for a user
+ */
+router.get('/libraries', async (req, res, next) => {
+    body = req.body
+    const reqFields = ['user_id']
+    let missingField = validator.validateRequiredFields(reqFields, body)
+
+    if (missingField) {
+        res.statusCode = 400
+        res.json({'error': `missing field ${missingField}`});
+    }
+
+    libraries = await db.getUserLibraries(body.user_id)
+    res.statusCode = 200
+    res.json({'libraries': libraries})
+});
+
+
+/**
+ * Add a user's book to their library of choice
+ */
+
 router.post('/books', async (req, res, next) => {
     // check the user is valid
-    const reqFields = ['user_id', 'lib_id', 'authors', 'title'] //subtitle, publish_date, publisher, edition, cover_img, genre
-    // iterate over authors to see if they exist in their tables
+    body = req.body
+    let reqFields = ['user_id', 'lib_id', 'authors', 'title', 'genre_id'] //subtitle, publish_date, publisher, edition, cover_img, genre
+    // 'authors' is an array of objects with fields: 'given_name', 'last_name' (optional)
+    let missingField = validator.validateRequiredFields(reqFields, body) 
+    
+    if (missingField) {
+        res.statusCode = 400
+        res.json({'error': `missing field ${missingField}`});
+    }
 
-    // check if book exists and if exists in intersection table
+    authors = body.authors
+    author_dict = {}
+    authors.forEach(async (item, index) => {
+        // validate each author object in the list
+        let reqFields = ['given_name']
+        missingField = validator.validateRequiredFields(reqFields, item)
+        if (missingField) {
+            res.statusCode = 400
+            res.json({'error': `missing field ${missingField}`});
+        }
+        // check if author exists
+        last_name = item.hasOwnProperty('last_name') ? item.last_name : ''
+        existingAuthors = await db.getAuthor(item.given_name, last_name)
+        if (existingAuthors.length === 0) {
+            // insert new author into table
+            author_id = await db.insertAuthor(item.given_name, last_name)
+            item.author_id = author_id
+        } else {
+            item.author_id = existingAuthors[0].author_id
+        }
+        author_dict[item.author_id] = item
+    });
 
-    // add book to library with status unread
+    // update book table
+
+    // get all books with this title
+    subtitle =  body.hasOwnProperty('subtitle') ? body.subtitle : '';
+    booksWithTitle = await db.booksByTitle(body.title, subtitle);
+
+    correctBookId = null
+    booksWithTitle.forEach(async (record, index) => {
+        bookAuthors = await db.getBookAuthors(record.book_id)
+        if (bookAuthors.length === author_dict.length) {
+            // check if match between author ids
+            unmatched = false;
+            bookAuthors.forEach((a) => {
+                if (! a.author_id in author_dict) {
+                    unmatched = true;
+                }
+            });
+            if (!unmatched) {
+                correctBookId = record.book_id
+        }
+       
+        }
+    });
+
+    if (!correctBookId) {
+        pubDate =  body.hasOwnProperty('pub_date') ? body.pub_date : null;
+        publisher =  body.hasOwnProperty('publisher') ? body.publisher : '';
+        edition = body.hasOwnProperty('edition') ? body.edition : null;
+        cover_url =  body.hasOwnProperty('cover_url') ? body.cover_url : '';
+        genre_id =  body.genre_id ;
+        correctBookId = await db.insertBook(body.title, subtitle, pubDate, publisher, edition, cover_url, genre_id)
+        // insert into books and book-author table
+
+        for (const [key, value] of Object.entries(author_dict)) {
+            await db.insertBookAuthor(correctBookId, key)
+        }
+        // author_dict.forEach(async (k, v) => {
+        //     await db.insertBookAuthor(correctBookId, k)
+        // });
+    }
+    // add to library with status unread
+    db.insertBookIntoLibrary(correctBookId, body.lib_id)
+    res.statusCode = 201
+    res.json({'status': 'success'})
 })
 
-
+/**
+ * Get all books in a library; optionally some
+ * filtering for matching some genre or read status
+ * in the future
+ */
 router.get('/books', async (req, res, next) => {
     try {
         let results = await db.all();
@@ -135,6 +238,28 @@ router.get('/books', async (req, res, next) => {
     }
 });
 
+/** Create a genre */
+
+router.post('/genres', async(req, res, next) => {
+    body = req.body
+    const reqFields = ['name']
+    let missingField = validator.validateRequiredFields(reqFields, body)
+
+    if (missingField) {
+        res.statusCode = 400
+        res.json({'error': `missing field ${missingField}`});
+    }
+
+    genre_id = await db.insertGenre(body.name)
+
+    res.statusCode = 201
+    res.json({'genre_id': genre_id})
+
+});
+
+/**
+ * Get information about a specific book in a user's library
+ */
 router.get('/books/:id', async(req, res, next) => {
     try {
         let results = await db.one(req.params.id);
@@ -145,6 +270,10 @@ router.get('/books/:id', async(req, res, next) => {
     } 
 })
 
+/**
+ * Update read status of a library book
+ */
+// TODO
 
 
 module.exports = router;
